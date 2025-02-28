2f363167bc21fef418fe7ff92bb06dab5ba03433
SmellyModder <41268323+SmellyModder@users.noreply.github.com>
2025-02-28 00:57:30 -0500

    Atoning for mistakes

--- src/main/java/com/teamabnormals/endergetic/core/other/tags/EEBiomeTags.java
+++ src/main/java/com/teamabnormals/endergetic/core/other/tags/EEBiomeTags.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.biome.Biome;
 
 public class EEBiomeTags {
 	public static final TagKey<Biome> HAS_EETLE = biomeTag("has_monster/eetle");
+	public static final TagKey<Biome> HAS_PURPOID = biomeTag("has_spawn/purpoid");
 	public static final TagKey<Biome> HAS_EETLE_NEST = biomeTag("has_structure/eetle_nest");
 	public static final TagKey<Biome> HAS_CORROCK = biomeTag("has_feature/corrock");
 	public static final TagKey<Biome> HAS_SPARSE_CORROCK = biomeTag("has_feature/sparse_corrock");
